246c948167f9a15295b33ce65e1aa3fcd17a168f
Murray Wynnes <murray.wynnes@bordeauxindex.com>
2023-07-28 17:41:14 +0100

    depl

---
+++ hooks/startup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+##
+# Scripts run from /home/site/wwwroot/
+# This is the root directory that is specified by /opt/startup/startup.sh
+##
+
+echo "Replacing Nginx config."
+
+cp hooks/default-dev /etc/nginx/sites-enabled/default
+service nginx restart
